ab94a43abdbcf7b3ad1575febfb277d94e2eac87
jasmineerina15 <jasmineerina15@gmail.com>
2024-05-29 13:48:26 +0700

    add declaration and matematika

---
+++ declaration/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	type mhsNIM string
+
+	var noNim mhsNIM = "19523095"
+
+	fmt.Println(noNim)
+}
---
+++ matematika.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a = 10
+	var b = 10
+	var d = 5
+	var e = 2
+	var c = a + b - d*e
+	fmt.Println(c)
+
+	var i = 10
+	i += 10 // i = i + 10
+	fmt.Println(i)
+
+	i += 5
+	fmt.Println(i)
+
+}
